2d15084795efd2e3cefe5fb135a6f5a091be09c9
nobo kumar <nobokumar227@gmail.com>
2024-03-23 00:19:14 +0600

    sketch area pixled

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const rows = 16;
 const column = 16;
 
 const sketchArea = document.querySelector ("#container");
-sketchArea.style.width = `${borderArea}`;
-sketchArea.style.height = `${borderArea}`;
+sketchArea.style.width = `${borderArea}px`;
+sketchArea.style.height = `${borderArea}px`;
 
 function grid (){
     for (let i = 0; i < (rows * column); i++ ){
         const gridPixel = document.createElement("div");
+        gridPixel.classList.add ("gridPixel")
+
         
         gridPixel.style.width = `${(borderArea / column) - 2}px`
         gridPixel.style.height = `${(borderArea / rows) - 2}px`
